582218e704ed77991a1ddab3c89f3ef5cc3312fc
Pabitra Pal <palr74502@gmail.com>
2023-08-05 23:03:34 +0530

    Add #30daysJavascriptChallange page
    this is a tracking page for this challenge, all projects and updates are uploaded here.....

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// name greetings function. .....,.................
+let person = prompt("Enter your name:", " Jone Doe")
+ console.log(person)
+const greet = document.getElementById("greeting")
+
+    greet.textContent = `Thank You ! 🙏 " ${person} " , For Visiting`;
+
+//  -------------------------------
+
+
