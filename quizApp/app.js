715b74952aa496fd7e5e200241524f6956697f7a
Pabitra Pal <palr74502@gmail.com>
2023-08-08 23:48:12 +0530

    Update app.js
    add cirtificate generator...

--- quizApp/app.js
+++ quizApp/app.js
@@ -1,4 +1,5 @@
 
+
 const questions = [
     {
     number : 1,
@@ -7,6 +8,7 @@ const questions = [
     ans2 : "Jordan",
     ans3 : "Havana",
     ans4 : "Austria",
+    ans : "3"
     },
     {
         number : 2,
@@ -15,6 +17,7 @@ const questions = [
         ans2 : "194",
         ans3 : "196",
         ans4 : "195",
+        ans : "4"
         },
         {
          number : 3,
@@ -23,7 +26,73 @@ const questions = [
         ans2 : "158 million KM",
         ans3 : "150 million KM",
          ans4 : "200 million KM",
+         ans : "3"
         }, 
+        {
+          number : 4,
+          que : "The great Victoria Desert is located in?",
+         ans1 : "Canada",
+         ans2 : "West Africa",
+         ans3 : "Australia",
+          ans4 : "North America",
+          ans : "3"
+         },  
+         {
+          number : 5,
+          que : "Which one of the five is least like the other four? BEAR - SNAKE - COW - DOG - TIGER",
+         ans1 : "Bear",
+         ans2 : "Snake",
+         ans3 : "Cow",
+          ans4 : "Dog",
+          ans : "1"
+         }, 
+         {
+          number: 6,
+          que:" Entomology is the science that studies?",
+          ans1: "Behavior of human beings",
+          ans2:"Insects",
+          ans3: "The origin and history of technical and scientific terms",
+          ans4: "The formation of rocks",
+          ans : "2",
+         },
+         {
+         number : 7,
+          que : "Golf player Vijay Singh belongs to which country?",
+         ans1 : "USA",
+         ans2 : "Fiji",
+         ans3 : "India",
+          ans4 : "UK",
+          ans : "2"
+         },
+         {
+          number : 8,
+           que : "Capital of Australia?",
+          ans1 : "Sydney",
+          ans2 : "Melbourne",
+          ans3 : "Canberra",
+           ans4 : "Victoria",
+           ans : "3"
+          }, 
+          {
+            number : 9,
+             que : "Capital of USA?",
+            ans1 : "New Yourk",
+            ans2 : "Chicago",
+            ans3 : "Washington,D.C.",
+             ans4 : "San Francisco",
+             ans : "3"
+            }, 
+            {
+              number : 10,
+               que : "World's Richest City?",
+              ans1 : "New York",
+              ans2 : "Tokyo",
+              ans3 : "Hong Kong",
+               ans4 : "London",
+               ans : "1"
+              }, 
+                           
+         
 
 ]
 
@@ -39,24 +108,48 @@ const massageArea = document.querySelector(".display-massage");
 const preBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const submitBtn = document.querySelector(".submit-btn");
-
+const cirtificateHideBtn = document.querySelector(".cancel-btn");
 // console.log({preBtn,nextBtn,submitBtn});
 // question structure............................
  let question = document.querySelector(".question-section")
- 
- 
+  
+ const cirtificate= document.querySelector(".complete");
+//  cirtificate.classList.remove("complete-hide");
 
+
+
+////////////////// collectin user name. /////////////
+const nameBtn = document.querySelector(".name-btn");
+let name = prompt("enter your name","");
+nameBtn.addEventListener('click',() =>{  
+    document.querySelector(".collect-name").value = name;
+    nameBtn.remove();
+})  
+
+
+
+
+//  inatials values ...............................................
 let currentScore = 0;
 // let currentTime = 
-let i = 0;
+let i = 0 ;
 
+//a all fucntions of app ===============================================================================================================================
+//  massegig for userName. 
+function worngansalert(msg){
+  massageArea.textContent = "OHH! Worng Answer😥";
+  massageArea.style.color = "red"; 
+}
+function rightansalert(){
+  massageArea.textContent = "HURRY! Correct Answer😍"
+  massageArea.style.color = "green";
+}
 
-// ===============================================================================================================================
 
 // seatting username .,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 // let name = prompt("enter your name here :" ,"");
-
-userName.textContent = name;
+// name = name.toUpperCase();
+userName.textContent = `" ${name} "`;
 
 const startUsername = document.querySelector(".start-username")
  startUsername.textContent = ` Welcome ! ${name}`
@@ -66,14 +159,17 @@ const startUsername = document.querySelector(".start-username")
 
  const startBtn = document.querySelector(".start-btn");
  const startPage = document.querySelector(".start-section");
+
  startBtn.addEventListener('click', () => {
     startPage.classList.add("start-section-hide");
     Createquestion(questions[0].number,questions[0].que,questions[0].ans1,questions[0].ans2,questions[0].ans3,questions[0].ans4);
+    console.log(name)
+    
  })
 
 
 
- // function creating new div and assign question on it.............
+ // function creating new div and assign question on it..........))))))))))))))))))))))))))))))))))))))))))
  function Createquestion(number,que,ans1,ans2,ans3,ans4){
 
     const newquestin = document.createElement('div');
@@ -85,19 +181,19 @@ const startUsername = document.querySelector(".start-username")
 <!-- </div> -->
 <form class="answers">
    <div class="ans" id="1">
-    <input class="check" type="checkbox" name="" id="">
+    <input class="check" type="checkbox" value="1" name="" id="">
     <label for="">${ans1}</label>
    </div>
    <div class="ans" id="2">
-    <input class="check" type="checkbox" name="" id="">
+    <input class="check" type="checkbox" value="2" name="" id="">
     <label for="">${ans2}</label>
    </div>
    <div class="ans" id="2">
-    <input class="check" type="checkbox" name="" id="">
+    <input class="check" type="checkbox" value="3" name="" id="">
     <label for="">${ans3}</label>
    </div>
    <div class="ans" id="2">
-    <input class="check" type="checkbox" name="" id="">
+    <input class="check" type="checkbox" value="4" name="" id="">
     <label for="">${ans4}</label>
    </div>
 </form>`
@@ -106,23 +202,101 @@ const startUsername = document.querySelector(".start-username")
 
  }
 //  Createquestion();
-//removing question before appering a new question.................
+
+//removing question before appering a new question. ))))))))))))))))))))))))))))))))))))))))))))))))))
 function Removequestion(){
   document.querySelector(".question-section").innerHTML = "";
 }
 
-// calculating score . .............
-function Calculatescore(){
 
+let ansi = 0;
+// calculating score ))))))))))))))))))))))))))))))))
+function Calculatescore(){
+//  for finding user answers .........................
+let inputvalue;
+// let userAns =[];
+const checkip = document.getElementsByClassName('check');
+ for(let i=0;checkip[i];i++){
+   if(checkip[i].checked){
+     inputvalue = checkip[i].value;
+     break;
+   }
+ }
+//  console.log(inputvalue)
+ let actualAns = questions[ansi++].ans;
+ console.log({actualAns,inputvalue})
+  if(actualAns === inputvalue){
+    currentScore +=10;
+    rightansalert();
+    setTimeout(() =>{
+      massageArea.textContent ="";
+    }, 1500);
+  }
+  else if(actualAns !== inputvalue){
+    worngansalert("OH! Worng Answer");
+    setTimeout(() =>{
+      massageArea.textContent ="";
+    }, 1500);
+  }
+  document.querySelector(".current-score").textContent = currentScore + "%";
 }
-// event listener ffor next question =========================================================
+
+//  function for citrificate showing .)))))))))))))))))))))))))))))))))))))))))))))))000000
+
+ function generateCirtificate(){
+
+     const date = new Date();
+     const yr = date.getFullYear();
+     const month = date.getMonth();
+     const day = date.getDay();
+  const dateSec = document.querySelector(".current-date");     
+  const scoreSec = document.querySelector(".cirtificate-score");
+  const nameSec = document.querySelector(".cirtificate-name");
+  const idSec = document.querySelector(".cirtificate-id");
+   
+  // assigning values ... to cirtificate ..........
+  scoreSec.textContent = currentScore + "% ";
+  nameSec.textContent = name;
+  idSec.textContent = " Cirtificate No: 0027ACR77290";
+  dateSec.textContent = day+"/"+month+"/"+yr;
+
+  // hide cirtificate.....
+  cirtificateHideBtn.addEventListener('click', () =>{
+    hideCirtificate();
+    restartQuiz();
+   })
+
+  
+ }
+
+ function hideCirtificate(){
+    cirtificate.classList.add("complete-hide");
+
+ }
+ function restartQuiz(){
+  currentScore = 0;
+ i=0;
+ }
+
+
+
+
+// =======================================================
+// event listener ffor next question ....................=========================================================
   nextBtn.addEventListener('click', () =>{
     Calculatescore();
+
+
     Removequestion();
     if(i === questions.length-1){
-        Shwoscore();
+        cirtificate.classList.remove("complete-hide");
+        generateCirtificate();
+        // console.log("ses");
+         
     }
      i++;
      Createquestion(questions[i].number,questions[i].que,questions[i].ans1,questions[i].ans2,questions[i].ans3,questions[i].ans4);
      
  })
+
+ 
